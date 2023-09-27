2871f0ef7248d30f43dea56fb8fdc44c7540762b
Mojtaba Rahmani <moj1x@hotmail.com>
2023-09-27 20:30:45 +0330

    Generate Db and Context

---
+++ Services/Catalog/Catalog.API/Data/CatalogCantext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogCantext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        public CatalogCantext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaeSettings:ConnectionStrings"));
+            // if db exist return it , when not exist created it
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaeSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaeSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+    }
+}
---
+++ Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> ProductCollection)
+        {
+            bool existProduct = ProductCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                ProductCollection.InsertManyAsync(GetSeedData());
+            }
+        }
+
+        private static IEnumerable<Product> GetSeedData()
+        {
+            return new List<Product>()
+          {
+              new Product()
+              {
+                  Id = "",
+                  Name ="",
+                  Category ="",
+                  Description ="",
+                  ImageName =  "",
+                  Price =1,
+                  Summery=""
+              },
+                 new Product()
+              {
+                  Id = "",
+                  Name ="",
+                  Category ="",
+                  Description ="",
+                  ImageName =  "",
+                  Price =2,
+                  Summery=""
+              }
+          };
+        }
+    }
+}
