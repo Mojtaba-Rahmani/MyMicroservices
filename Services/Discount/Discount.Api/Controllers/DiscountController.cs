7d0b22f901e14dcabe720d8cd85a91874a4661ed
Mojtaba Rahmani <moj1x@hotmail.com>
2023-10-20 20:21:58 +0330

    Create Discount with Postgres DB

---
+++ Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,89 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : Controller
+    {
+        #region Constructor
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        #endregion
+
+        #region Get Coupon
+
+        [HttpGet("{ProductName}" , Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string ProductName)
+        {
+            var coupon = await _discountRepository.GetDiscount(ProductName);
+
+            return Ok(coupon);  
+        }
+
+        #endregion
+
+        #region Create Coupon
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _discountRepository.CreateDiscount(coupon);
+
+            return CreatedAtRoute("GetDiscount", new
+            {
+                ProductName = coupon.ProductName
+            },coupon);
+
+        }
+
+        #endregion
+
+        #region 
+
+        #region Edit Coupon
+
+        [HttpPut]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+          return Ok(await _discountRepository.UpdateDiscount(coupon));
+
+        }
+
+        #endregion
+
+        #endregion Delete Coupon
+
+        [HttpDelete("{ProductName}")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string ProductName)
+        {
+            return Ok(await _discountRepository.DeleteDiscount(ProductName));
+
+        }
+
+        #region 
+
+
+
+        #endregion
+
+        #region 
+
+
+
+        #endregion
+    }
+}
---
+++ Services/Discount/Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,119 @@
+﻿using Discount.Api.Entities;
+using System.Threading.Tasks;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+namespace Discount.Api.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        #region Constructor
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        #endregion
+
+
+        #region Get Coupon
+        public async Task<Coupon> GetDiscount(string ProductName)
+        {
+            using var connection = new NpgsqlConnection(
+                                _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var coupon = await connection.QueryFirstOrDefaultAsync
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName"
+                , new { ProductName = ProductName });
+
+            if (coupon == null)
+            {
+                return new Coupon
+                {
+                    ProductName = "No Discount"
+                    ,
+                    Amount = 0
+                    ,
+                    Description = "No Discount Description"
+                };
+            }
+
+            return coupon;
+
+        }
+        #endregion
+
+        #region Create Coupon
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(
+                      _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await connection.ExecuteAsync
+                (
+                "INSERT INTO Coupon (ProductName,Description,Amount) " +
+                "VALUES(@ProductName,@Description,@Amount)"
+                , new
+                {
+                    ProductName = coupon.ProductName,
+                    Description = coupon.Description,
+                    Amount = coupon.Amount
+                }
+                );
+            if (affected == 0)
+                return false;
+            else
+                return true;
+        }
+        #endregion
+
+        #region Update Coupon
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection(
+              _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await connection.ExecuteAsync
+                (
+                "UPDATE Coupon SET ProductName=@ProductName, Description=@Description,Amount=@Amount"
+                , new
+                {
+                    ProductName = coupon.ProductName,
+                    Description = coupon.Description,
+                    Amount = coupon.Amount
+                }
+                );
+
+            if (affected == 0)
+                return false;
+            else
+                return true;
+
+        }
+        #endregion
+
+        #region Delete Coupon
+        public async Task<bool> DeleteDiscount(string ProductName)
+        {
+            using var connection = new NpgsqlConnection(
+         _configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await connection.ExecuteAsync
+                (
+                "DELETE FROM Coupon WHERE ProductName=@ProductName"
+                , new{ ProductName = ProductName }
+                );
+
+            if (affected == 0)
+                return false;
+            else
+                return true;
+        }
+        #endregion
+
+
+
+    }
+}
