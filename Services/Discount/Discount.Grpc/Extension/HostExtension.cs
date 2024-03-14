85c6d57a2c93ead9815b7fc85998d05bae01219f
Mojtaba Rahmani <moj1x@hotmail.com>
2024-03-15 01:18:18 +0330

    Add GRPC

---
+++ Services/Discount/Discount.Grpc/Extension/HostExtension.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Threading;
+
+namespace Discount.Grpc.Extension
+{
+    public static class HostExtension
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                // this is configuration in startup
+                // exampl:
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                // Migration Database
+
+                try
+                {
+                    logger.LogInformation("migrating postgtresql database");
+                    
+                    using var connection = new NpgsqlConnection(
+                        configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection,
+                    };
+
+                   
+
+                    command.CommandText = "Drop Table If Exists Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName VARCHAR(200) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+
+
+
+
+
+                    command.ExecuteNonQuery();
+
+                    // seed data
+
+                    command.CommandText = "Insert Into Coupon(ProductName,Description,Amount) Values('IPhone x1','Iphone discount',150)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "Insert Into Coupon(ProductName,Description,Amount) Values('Sumsung 10','Sumsung discount',250)";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("migration has been complated");
+
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError("an error has been ocured");
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
---
+++ Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,42 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        #region Constructor
+        private readonly IDiscountRepository _discountRepository;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository discountRepository , ILogger<DiscountService> logger)
+        {
+            _discountRepository = discountRepository;
+            _logger = logger;
+        }
+        #endregion
+
+
+        #region GetDiscount
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _discountRepository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound,$"Discount with ProductName {request.ProductName} is not found"));
+            }
+
+            return new CouponModel
+            {
+                Id = coupon.Id,
+                ProductName = coupon.ProductName,
+                Amount = coupon.Amount,
+                Description = coupon.Description
+            };
+        }
+
+        #endregion
+    }
+}
